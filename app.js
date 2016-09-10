7a5aa0bd9f33a428685238f150c1da288f1b5fa9
Dima Naumchik <ceqadrat@gmail.com>
2016-09-10 19:23:02 +0300

    Make first request

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-/**
- * Created by Dima on 10.09.2016.
- */
+const http = require("http");
+
+
+let options = {
+    hostname: 'coldfilm.ru',
+    port: 80,
+    path: '/',
+    method: 'GET'
+};
+
+let request = http.request(options, (res) => {
+    res.setEncoding('utf-8');
+    res
+        .on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        })
+        .on('end', () => {
+            console.log('end')
+        })
+});
+
+request.on('error', (err) => {
+    console.error('123', err);
+});
+
+request.end();
